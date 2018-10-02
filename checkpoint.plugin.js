14f01c95fbde19d47013a576301769701ef7a1ac
Rick Rothenberg <rick@cloud-elements.com>
2018-10-02 10:12:18 -0600

    Just print out the important info

--- checkpoint.plugin.js
+++ checkpoint.plugin.js
@@ -7,23 +7,16 @@ module.exports = function ({ types: t }) {
         if (path.node.callee.name !== '$checkpoint' || path.parentPath.parent.type !== 'SwitchCase') {
           return;
         }
-        console.log('---------CallExpression-----------')
-        console.log(path);
         //path.findParent(path => {console.log('--- parent path ---');console.log(path); return !path})
         const blockPath = path.findParent(path => path.parentPath.node.type === 'BlockStatement' && path.parentPath.parent.type === 'FunctionExpression')
-        console.log('--- blockPath ---');
-        console.log(blockPath);
-        console.log(blockPath.node.body[0])
         blockPath.addComment(
           'leading',
           ' TODO need to recognize when the context is being passed in and use it and also set the variables',
           true)
+        console.log('scope:', Object.keys(blockPath.scope.parent.bindings))
+        console.log('scope:', Object.keys(blockPath.parentPath.scope.parent.bindings))
         const a2gPath = path.findParent(path => path.node.callee && path.node.callee.name === '_asyncToGenerator');
-        console.log('--- a2gPath ---');
-        console.log(a2gPath);
         const functionPath = a2gPath.findParent(path => path.node.callee && path.node.callee.type === 'FunctionExpression');
-        console.log('--- functionPath ---');
-        console.log(functionPath);
         const functionName = functionPath.parent.id.name;
         console.log('functionName:', functionName)
         const topPath = functionPath.findParent(path => path.node.type === 'Program')

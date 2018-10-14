b53db0b41fc2a69c1a3e2ea8649bb60de77fc406
Rick Rothenberg <rick@cloud-elements.com>
2018-10-14 09:38:50 -0600

    Fixed continuation behavior

--- serverlessCheckpointer.js
+++ serverlessCheckpointer.js
@@ -3,20 +3,23 @@ const pako = require('pako');
 module.exports.wrapper = function(handler) {
   let globalScope = {continuing: false, local: true}
   return async function () {
-    console.log('wrapper', arguments)
+    console.log('wrapper', arguments[0])
     let done = false;
     let stack = {}
     while (!done) {
       try {
         return await handler(...arguments, {globalScope, stack: stack})
       } catch (e) {
-        console.log('caught', e)
         if (e.type === 'checkpoint') {
           done = !globalScope.local;
           globalScope.continuing = true;
           const serializedState = pako.inflate(e.state, {to: 'string'})
           stack = JSON.parse(serializedState).stack
+          if (!done) {
+            console.log('Restarting with state for', Object.keys(stack).join(', '))
+          }
         } else {
+          console.log('caught', e)
           throw e;
         }
       }
@@ -30,6 +33,7 @@ module.exports.continuing = function(state) {
 
 module.exports.buildState = function(state, functionName, context) {
   if (state.globalScope.continuing) {
+    delete state.stack[functionName]
     return state
   } else {
     return {globalScope: state.globalScope, stack: Object.assign({[functionName]: context}, state.stack)}
@@ -41,8 +45,13 @@ module.exports.checkpoint = function() {
   const serializedState = JSON.stringify(state);
   const compressedState = pako.deflate(serializedState, {to: 'string'})
   if (state.globalScope.continuing) {
-    state.globalScope.continuing = false;
+    console.log('checkpoint', arguments[0], 'continuing...')
+    if (Object.keys(state.stack).length === 0) {
+      console.log('checkpoint continuation completed')
+      state.globalScope.continuing = false;
+    }
   } else {
+    console.log('checkpoint', arguments[0], 'reached')
     throw {type: 'checkpoint', state: compressedState}
   }
 }
@@ -54,6 +63,7 @@ module.exports.getState = function (args) {
 }
 
 module.exports.restoreState = function(context, contextName, functionName, state) {
+  console.log('restoring state for', functionName)
   context.next = state.stack[functionName][contextName].next;
   context.prev = state.stack[functionName][contextName].prev;
   state.stack[functionName][contextName] = context;

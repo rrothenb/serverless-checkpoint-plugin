a592836df8f1e8c6201f7ee07f34bb01fbae98f9
Rick Rothenberg <rick@cloud-elements.com>
2018-11-02 17:31:21 -0600

    Add completion estimation

--- serverlessCheckpointer.js
+++ serverlessCheckpointer.js
@@ -16,14 +16,16 @@ function replacer(key, value) {
 }
 
 module.exports.wrapper = function(handler) {
+  console.log('Starting at', `${new Date()}`)
   let globalScope = {continuing: false, local: true}
   return async function () {
-    console.log('wrapper', arguments[0])
     let done = false;
     let stack = {}
     while (!done) {
       try {
-        return await handler(...arguments, {globalScope, stack: stack})
+        const response = await handler(...arguments, {globalScope, stack: stack})
+        console.log('Completed at', `${new Date()}`)
+        return response
       } catch (e) {
         if (e.type === 'checkpoint') {
           done = !globalScope.local;
@@ -32,7 +34,6 @@ module.exports.wrapper = function(handler) {
           stack = parse(serializedState, reviver).stack
           if (!done) {
             console.log('Restarting with state for', Object.keys(stack).join(', '))
-            console.log('state size:', serializedState.length, 'compressed size:', e.state.length)
           }
         } else {
           console.log('caught', e)
@@ -61,9 +62,31 @@ module.exports.checkpoint = function() {
     console.log('checkpoint', arguments[0], 'continuing...')
     state.globalScope.continuing = false;
   } else {
+    if (!state.stack['$checkpoint']) {
+      state.stack['$checkpoint'] = {[arguments[0]]: {start: new Date(), done: 0}}
+      console.log(`Reached ${arguments[0]} checkpoint`)
+    } else {
+      if (state.stack['$checkpoint'][arguments[0]]) {
+        state.stack['$checkpoint'][arguments[0]].done++
+        const numDone = state.stack['$checkpoint'][arguments[0]].done
+        const numTotal = arguments[1]
+        const numRemaining = numTotal - numDone
+        const percentDone = Math.round(numDone*100/numTotal)
+        const timeElapsed = new Date() - new Date(state.stack['$checkpoint'][arguments[0]].start)
+        const estimateRemainingTime = Math.round(timeElapsed*numRemaining/numDone)
+        const estimatedCompletionDate = new Date(new Date().getTime() + estimateRemainingTime)
+        console.log(
+          `Reached next ${arguments[0]} checkpoint.`,
+          `${percentDone}% done.`,
+          `Should finish by ${estimatedCompletionDate}`)
+      } else {
+        state.stack['$checkpoint'] = {[arguments[0]]: {start: new Date(), done: 0}}
+        console.log(`Reached ${arguments[0]} checkpoint`)
+      }
+    }
     const serializedState = stringify(state, replacer);
     const compressedState = pako.deflate(serializedState, {to: 'string'})
-    console.log('checkpoint', arguments[0], 'reached - state size:', serializedState.length, 'compressed size:', compressedState.length)
+    console.debug('state size:', serializedState.length, 'compressed size:', compressedState.length)
     throw {type: 'checkpoint', state: compressedState}
   }
 }
@@ -75,7 +98,6 @@ module.exports.getState = function (args) {
 }
 
 module.exports.restoreState = function(context, contextName, functionName, state) {
-  console.log('restoring state for', functionName)
   context.next = state.stack[functionName][contextName].prev;
   context.prev = state.stack[functionName][contextName].prev;
   context.sent = state.stack[functionName][contextName].sent;
